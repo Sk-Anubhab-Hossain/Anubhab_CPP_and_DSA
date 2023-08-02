d669e10d85cb56b0fc9a1937056da7e04cb942a9
SK ANUBHAB HOSSAIN <anubhabhossain22@gmail.com>
2023-08-02 19:51:54 +0530

    first Commit

---
+++ C++ programs/c++ Lec1.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a,b,sum;
+//	float c;
+//	double d;
+//	a=10;
+//	b=37;
+//	c=6.897970;
+//	d=8.78666456588575;
+//	cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl;
+//	cout<<"hello world"<<endl<< "hii";
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a,b,sum;
+//	cout<<"enter first number:";
+//	cin>>a;
+//	cout<<"enter second number:";
+//	cin>>b;
+//	sum=a+b;
+//	cout<<"sum is:"<<sum<<endl;
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,sum;
+	cout<<"enter first number:";
+	cin>>a;
+	cout<<"enter second number:";
+	cin>>b;
+	sum=a+b;
+	cout<<"sum is:"<<sum<<endl;
+	cout<<"Difference is:"<<a-b<<endl;
+	cout<<"Product is:"<<a*b<<endl;
+	cout<<"Division is:"<<a/b<<endl;
+	cout<<"remainder is:"<<a%b<<endl;
+	return 0;
+}
